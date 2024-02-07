e527b07f3171001182acb1c3a98293d08278da2e
Bryan Miller <millerwbryan@gmail.com>
2024-02-07 09:10:07 -0300

    Latest changes

--- fermat_spiral.py
+++ fermat_spiral.py
@@ -6,32 +6,34 @@
 
 import numpy as np
 
+
 # Calculate points in a Fermat spiral
-def fermat(n_point,a=0.03,b=1.0,theta=2.399963):
+def fermat(n_point, a=0.03, b=1.0, theta=2.399963):
     # local variable with the correct type
-    l_npt = int(n_point)
-    l_a = float(a)
-    l_b = float(b)
-    l_theta = float(theta)
+    l_npt = np.int16(n_point)
+    l_a = np.float32(a)
+    l_b = np.float32(b)
+    l_theta = np.float32(theta)
 
     x = np.zeros(n_point)
     y = np.zeros(n_point)
 
     for ii in range(l_npt):
-        l_ii = float(ii)
+        l_ii = np.float32(ii)
         x[ii] = l_a*np.sqrt(l_ii)*np.cos(l_b*l_ii*l_theta)
         y[ii] = l_a*np.sqrt(l_ii)*np.sin(l_b*l_ii*l_theta)
 
-    return x,y
+    return x, y
+
 
 # Determine scale factor needed to produce a given offset in n_point points
-def fermat_scale(n_point,offset,b=1.0,theta=2.399963):
+def fermat_scale(n_point, offset, b=1.0, theta=2.399963):
     # local variable with the correct type
-    l_npt = float(n_point)
-    l_offset = float(offset)
-    l_b = float(b)
-    l_theta = float(theta)
+    l_npt = np.float32(n_point)
+    l_offset = np.float32(offset)
+    l_b = np.float32(b)
+    l_theta = np.float32(theta)
 
-    a = l_offset/(np.sqrt(l_npt*(np.cos(l_b*l_npt*l_theta)**2 + np.sin(l_b*l_npt*l_theta)**2)))
+    a = l_offset / (np.sqrt(l_npt*(np.cos(l_b*l_npt*l_theta)**2 + np.sin(l_b*l_npt*l_theta)**2)))
 
     return a
--- fermat_test.py
+++ fermat_test.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# Fermat spiral tests
+# Bryan Miller
+
 from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
@@ -28,30 +31,31 @@ if __name__ == "__main__":
     # print(a,b)
     dmax = args.d
     nnew = args.p
-
+    
     # Sigma (arcsec) for normal randomization
     r = args.r
 
     # golden angle
-    theta=137.50776*np.pi/180.
+    theta = 137.50776 * np.pi / 180.
     # print(theta)
 
     #np = 33
     n = args.n
     # print(n)
-    x,y = fermat(n,a=a,b=b,theta=theta)
+    x,y = fermat(n, a=a, b=b, theta=theta)
     # convert from degrees to arcsec
     x = 3600.*x
     y = 3600.*y
 
     r = np.random.normal(0.0, r, (n,2))
 
+	# Compute offset positions based on spiral parameters
     for ii in range(n):
         x[ii] = x[ii] + r[ii,0]
         y[ii] = y[ii] + r[ii,1]
         offset = 0.0
         if ii > 0:
-            offset = np.sqrt((x[ii] - x[ii - 1]) ** 2 + (y[ii] - y[ii - 1]) ** 2)
+            offset = np.sqrt((x[ii] - x[ii - 1])**2 + (y[ii] - y[ii - 1])**2)
         dist = np.sqrt((x[ii])**2 + (y[ii])**2)  # distance from origin
         print('{:7.2f} {:7.2f} {:7.2f} {:7.2f}'.format(x[ii], y[ii], offset, dist))
 
@@ -59,14 +63,16 @@ if __name__ == "__main__":
     plt.show()
 
     # Test scale
-    if dmax == 0:
+    if dmax == 0.0:
         dmax = dist
     if nnew == 0:
         nnew = n
-    dmax = dist # arcsec
-    anew = fermat_scale(nnew,dmax/3600.,b=b,theta=theta)
-    print('a({:3d}, {:7.2f}) = {:7.3f}'.format(nnew,dmax,anew))
-    x,y = fermat(nnew,a=anew,b=b,theta=theta)
+
+	# Determine scale factor a from maximum offset and number of positions
+    anew = fermat_scale(nnew, dmax/3600., b=b, theta=theta)
+    print('a({:3d}, {:7.2f}) = {:7.5f}'.format(nnew, dmax, anew))
+
+    x,y = fermat(nnew, a=anew, b=b, theta=theta)
     # convert from degrees to arcsec
     x = 3600.*x
     y = 3600.*y
@@ -75,6 +81,6 @@ if __name__ == "__main__":
         y[ii] = y[ii] + r[ii,1]
         offset = 0.0
         if ii > 0:
-            offset = np.sqrt((x[ii] - x[ii - 1]) ** 2 + (y[ii] - y[ii - 1]) ** 2)
+            offset = np.sqrt((x[ii] - x[ii - 1])**2 + (y[ii] - y[ii - 1])**2)
         dist = np.sqrt((x[ii])**2 + (y[ii])**2)  # distance from origin
         print('{:7.2f} {:7.2f} {:7.2f} {:7.2f}'.format(x[ii], y[ii], offset, dist))
